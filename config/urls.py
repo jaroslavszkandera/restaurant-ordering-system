aa1d0a3bb7018c3ec7e40bd4101a4eecb0cdd136
jaroslavszkandera <jaroslav.szk@gmail.com>
2025-05-25 17:33:43 +0800

    Fix typo

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("orders.urls")),
-    path("acconts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 # Serve media files in development
